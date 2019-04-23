8c16ebf988dfa6fbffb50c2c101059f240a221fe
addcaa <3152185832@qq.com>
2019-04-23 17:17:33 +0800

    bul

--- app/Http/Controllers/Jssdk/JssdkController.php
+++ app/Http/Controllers/Jssdk/JssdkController.php
@@ -13,16 +13,19 @@ class JssdkController extends Controller{
         $nonceStr=Str::random(10);
         $timestamp=time();
         $url=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-        $res="jsapi_ticket$jsapi_ticket&noncestr$nonceStr&timestamp$timestamp&url$url";
-        //签名
-        $signature=sha1($res);
+        // $res="jsapi_ticket$jsapi_ticket&noncestr$nonceStr&timestamp$timestamp&url$url";
+        // //签名
+        // $signature=sha1($res);
+        $string1 = "jsapi_ticket=$jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+        $sign= sha1($string1);
+        // dd($sign);
         $arr=[
             'appId'=>env('WX_APP_ID'),    // 必填，公众号的唯一标识
             'timestamp'=>$timestamp,     // 必填，生成签名的时间戳
             'nonceStr'=>$nonceStr,       // 必填，生成签名的随机串
-            'signature'=>$signature     // 必填，签名
+            'signature'=>$sign     // 必填，签名
         ];
-        // dd($arr);
+        dd($arr);
         return view('jssdk/jssdk',['arr'=>$arr]);
     }
 }
--- resources/views/jssdk/jssdk.blade.php
+++ resources/views/jssdk/jssdk.blade.php
@@ -15,7 +15,7 @@
     wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。 -->
         appId:"{{$arr['appId']}}",
-        timestamp:"{{$arr['timestamp']}}" ,
+        timestamp:"{{$arr['timestamp']}}",
         nonceStr: "{{$arr['nonceStr']}}",
         signature: "{{$arr['signature']}}",
         jsApiList: ['chooseImage']
