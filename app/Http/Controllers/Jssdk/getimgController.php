c12b57e32d02d80450c9e5a960b28965ab51e265
addcaa <3152185832@qq.com>
2019-04-23 17:43:47 +0800

    debug

---
+++ app/Http/Controllers/Jssdk/getimgController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Jssdk;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class getimgController extends Controller
+{
+    public function jssdk(){
+        dd($_GET);
+    }
+}
