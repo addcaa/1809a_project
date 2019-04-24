9a5c834f018c5de6db005b7fa0ac18c144aec40b
addcaa <3152185832@qq.com>
2019-04-24 18:27:32 +0800

    微信

--- app/Http/Controllers/Jssdk/JssdkController.php
+++ app/Http/Controllers/Jssdk/JssdkController.php
@@ -56,17 +56,19 @@ class JssdkController extends Controller{
         $access_info="https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid&lang=zh_CN";
         $user=json_decode(file_get_contents($access_info),true);
         // print_r($user);
-        $info=[
-            'openid'=>$user['openid'],
-            'name'=>$user['nickname'],
-            'headimgurl'=>$url['headimgurl']
-        ];
-        $wx_user=WxUserModel::insert($info);
-        if($wx_user){
-            echo "成功";
-        }else{
-            echo "失败";
-        }
+        $log_str = date('Y-m-d H:i:s') . "\n" . $user . "\n<<<<<<<";
+        file_put_contents('logs/wx_user_notice.log',$log_str,FILE_APPEND);
+        // $info=[
+        //     'openid'=>$user['openid'],
+        //     'name'=>$user['nickname'],
+        //     'headimgurl'=>$url['headimgurl']
+        // ];
+        // $wx_user=WxUserModel::insert($info);
+        // if($wx_user){
+        //     echo "成功";
+        // }else{
+        //     echo "失败";
+        // }
 
     }
 }
