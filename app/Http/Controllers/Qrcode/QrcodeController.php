9a705caac0ffb2d9366b70060f49b58ff31225d6
addcaa <3152185832@qq.com>
2019-04-28 17:39:57 +0800

    转发

--- app/Http/Controllers/Qrcode/QrcodeController.php
+++ app/Http/Controllers/Qrcode/QrcodeController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use function GuzzleHttp\json_decode;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
 class QrcodeController extends Controller
 {
     //创建二维码ticket
@@ -40,7 +42,23 @@ class QrcodeController extends Controller
     }
     /**商品转发 */
     public function goodsget(Request $request){
-        $wxconfig=$request->signPackage;
+        // $wxconfig=$request->signPackage;
+        $jsapi_ticket=jsapi_ticket();
+        $nonceStr=Str::random(10);
+        $timestamp=time();
+        $url=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        // $res="jsapi_ticket$jsapi_ticket&noncestr$nonceStr&timestamp$timestamp&url$url";
+        // //签名
+        // $signature=sha1($res);
+        $string1 = "jsapi_ticket=$jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+        $sign= sha1($string1);
+        // dd($sign);
+        $wxconfig=[
+            'appId'=>env('WX_APP_ID'),    // 必填，公众号的唯一标识
+            'timestamp'=>$timestamp,     // 必填，生成签名的时间戳
+            'nonceStr'=>$nonceStr,       // 必填，生成签名的随机串
+            'signature'=>$sign     // 必填，签名
+        ];
         // dd($wxconfig);
         $arr=DB::table('goods')->where(['goods_id'=>8])->get()->toArray();
         // dd($arr);
